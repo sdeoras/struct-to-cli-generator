53598383a8cde2e8cb9ab1d5c298efac87c5a7e9
Saurabh Deoras <sdeoras@gmail.com>
2018-02-18 07:49:09 -0800

    code update

--- cli-code-gen/main.go
+++ cli-code-gen/main.go
@@ -4,15 +4,17 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/libopenstorage/openstorage/osdconfig"
 	"io/ioutil"
 	"log"
 	"os"
 	"reflect"
 	"strings"
+
+	"github.com/libopenstorage/openstorage/osdconfig"
 )
 
 var prefix2VarName map[string]string
+var prefixOrigin map[string]string
 var cw *bufio.Writer
 var fw *bufio.Writer
 var header = `package main
@@ -20,41 +22,74 @@ var header = `package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/Sirupsen/logrus"
 	"github.com/libopenstorage/openstorage/osdconfig"
 	"github.com/urfave/cli"
 	"io/ioutil"
 	"os"
 )
 
-var config *osdconfig.ClusterConfig
-var nodeConfig *osdconfig.NodeConfig
+type manager struct{}
 
-func main() {
-	config = new(osdconfig.ClusterConfig)
-	nodeConfig = new(osdconfig.NodeConfig)
-
+func (m *manager) GetClusterConf() (*osdconfig.ClusterConfig, error) {
+	config := new(osdconfig.ClusterConfig)
 	if b, err := ioutil.ReadFile("/tmp/config.json"); err != nil {
-		logrus.Warn(err)
+		return nil, err
 	} else {
 		if err := json.Unmarshal(b, config); err != nil {
-			logrus.Warn(err)
+			return nil, err
 		}
 	}
-
+	return config, nil
+}
+func (m *manager) GetNodeConf() (*osdconfig.NodeConfig, error) {
+	config := new(osdconfig.NodeConfig)
 	if b, err := ioutil.ReadFile("/tmp/nodeConfig.json"); err != nil {
-		logrus.Warn(err)
+		return nil, err
 	} else {
-		if err := json.Unmarshal(b, nodeConfig); err != nil {
-			logrus.Warn(err)
+		if err := json.Unmarshal(b, config); err != nil {
+			return nil, err
 		}
 	}
+	return config, nil
+}
+func (m *manager) SetClusterConf(config *osdconfig.ClusterConfig) error {
+	if b, err := json.Marshal(config); err != nil {
+		return err
+	} else {
+		if err := ioutil.WriteFile("/tmp/config.json", b, 0666); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (m *manager) SetNodeConf(config *osdconfig.NodeConfig) error {
+	if b, err := json.Marshal(config); err != nil {
+		return err
+	} else {
+		if err := ioutil.WriteFile("/tmp/nodeConfig.json", b, 0666); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var clusterManager *manager
+
+func main() {
+	clusterManager = new(manager)
 
 	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "json, j",
+			Usage: "Print JSON output",
+		},
+	}
 	app.Commands = []cli.Command{`
 
 func main() {
 	prefix2VarName = make(map[string]string)
+	prefixOrigin = make(map[string]string)
 	config := new(osdconfig.ClusterConfig)
 	config.Secrets = new(osdconfig.SecretsConfig)
 	config.Kvdb = new(osdconfig.KvdbConfig)
@@ -69,10 +104,21 @@ func main() {
 
 	fmt.Fprintln(cw, header)
 	prefix2VarName["config"] = "config"
+	prefixOrigin["config"] = "config"
 	printFields(reflect.Indirect(reflect.ValueOf(config)), false, "config", "usage", "description", "\t\t")
 	fmt.Fprintln(cw, tabs("\t", 0), "}")
 	fmt.Fprintln(cw, tabs("\t", 0), "app.Run(os.Args)")
 	fmt.Fprintln(cw, "}")
+
+	fmt.Fprintln(fw, "func printJson(obj interface{}) error {")
+	fmt.Fprintln(fw, "\t", "if b, err := json.MarshalIndent(obj, \"\", \"  \"); err != nil {")
+	fmt.Fprintln(fw, "\t\t", "return err")
+	fmt.Fprintln(fw, "\t", "} else {")
+	fmt.Fprintln(fw, "\t\t", "fmt.Println(string(b))")
+	fmt.Fprintln(fw, "\t\t", "return nil")
+	fmt.Fprintln(fw, "\t", "}")
+	fmt.Fprintln(fw, "}")
+	fmt.Fprintln(fw)
 
 	cw.Flush()
 	fw.Flush()
@@ -80,6 +126,49 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("wrote cli.go")
+}
+
+// define a function for getter field behavior
+func getFunc(cw, fw *bufio.Writer, v reflect.Value, i int, tab, prefix string) {
+	if !isEnabled(v.Type(), i) {
+		return
+	}
+	fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
+	fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\""+getTag(v.Type(), i)+"\",")
+
+	fmt.Fprintln(fw, "\t", "if c.IsSet(\"all\") || c.IsSet(\""+getTag(v.Type(), i)+"\") {")
+	fmt.Fprintln(fw, "\t\t", "fmt.Println(", "\""+v.Type().Field(i).Name+":\",", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, ")")
+	fmt.Fprintln(fw, "\t", "}")
+
+	fmt.Fprintln(cw, tabs(tab, 5), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
+	if isHidden(v.Type(), i) {
+		fmt.Fprintln(cw, tabs(tab, 5), "Hidden: true,")
+	} else {
+		fmt.Fprintln(cw, tabs(tab, 5), "Hidden: false,")
+	}
+	fmt.Fprintln(cw, tabs(tab, 4), "},")
+}
+
+// define a function  for setter field behavior
+func setFunc(cw, fw *bufio.Writer, v reflect.Value, i int, tab, prefix, flagType, flagTag string) {
+	if !isEnabled(v.Type(), i) {
+		return
+	}
+	fmt.Fprintln(cw, tabs(tab, 2), "cli."+flagType+"Flag{")
+	fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+getTag(v.Type(), i)+"\",")
+
+	fmt.Fprintln(fw, "\t", "if c.IsSet(\""+getTag(v.Type(), i)+"\") {")
+	fmt.Fprintln(fw, "\t\t", getCamelCase(getTag(v.Type(), i)), ":=", "c."+flagType+"(\""+getTag(v.Type(), i)+"\")")
+	fmt.Fprintln(fw, "\t\t", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, " = ", getCamelCase(getTag(v.Type(), i)))
+	fmt.Fprintln(fw, "\t", "}")
+
+	fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"("+flagTag+")\\t"+getUsage(v.Type(), i)+"\",")
+	if isHidden(v.Type(), i) {
+		fmt.Fprintln(cw, tabs(tab, 3), "Hidden: true,")
+	} else {
+		fmt.Fprintln(cw, tabs(tab, 3), "Hidden: false,")
+	}
+	fmt.Fprintln(cw, tabs(tab, 2), "},")
 }
 
 func printFields(v reflect.Value, hidden bool, prefix, usage, description, tab string) {
@@ -90,109 +179,55 @@ func printFields(v reflect.Value, hidden bool, prefix, usage, description, tab s
 	fmt.Fprintln(cw, tabs(tab, 1), "Hidden:", hidden, ",")
 	fmt.Fprintln(cw, tabs(tab, 1), "Action:", getCamelCase("set_"+prefix+"_values,"))
 
-	fmt.Fprintln(fw, "func", getCamelCase("set_"+prefix+"_values(c *cli.Context) {"))
+	fmt.Fprintln(fw, "func", getCamelCase("set_"+prefix+"_values(c *cli.Context) error {"))
+	if prefixOrigin[prefix] == "node" {
+		fmt.Fprintln(fw, "\t", "config, err := clusterManager.GetNodeConf()")
+		fmt.Fprintln(fw, "\t", "if err != nil {")
+		fmt.Fprintln(fw, "\t\t", "return err")
+		fmt.Fprintln(fw, "\t", "}")
+	} else {
+		fmt.Fprintln(fw, "\t", "config, err := clusterManager.GetClusterConf()")
+		fmt.Fprintln(fw, "\t", "if err != nil {")
+		fmt.Fprintln(fw, "\t\t", "return err")
+		fmt.Fprintln(fw, "\t", "}")
+	}
 	fmt.Fprintln(cw, tabs(tab, 1), "Flags: []cli.Flag{")
-	if prefix == "node" {
+	/*if prefix == "node" {
 		fmt.Fprintln(cw, tabs(tab, 2), "cli.StringSliceFlag{")
 		fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+"id"+"\",")
 		fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"(Str...)\\tNode id\",")
 		fmt.Fprintln(cw, tabs(tab, 2), "},")
-	}
+	}*/
+
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
 		switch k := field.Kind(); k {
 		case reflect.Slice:
 			switch field.Type() {
 			case reflect.TypeOf([]string{}):
-				fmt.Fprintln(cw, tabs(tab, 2), "cli.StringSliceFlag{")
-				fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+getTag(v.Type(), i)+"\",")
+				setFunc(cw, fw, v, i, tab, prefix, "StringSlice", "Str...")
 
-				fmt.Fprintln(fw, "\t", "if c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-				fmt.Fprintln(fw, "\t\t", getCamelCase(getTag(v.Type(), i)), ":=", "c.StringSlice(\""+getTag(v.Type(), i)+"\")")
-				fmt.Fprintln(fw, "\t\t", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, " = ", getCamelCase(getTag(v.Type(), i)))
-				fmt.Fprintln(fw, "\t", "}")
-
-				fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"(Str...)\\t"+getUsage(v.Type(), i)+"\",")
-				if isHidden(v.Type(), i) {
-					fmt.Fprintln(cw, tabs(tab, 3), "Hidden: true,")
-				} else {
-					fmt.Fprintln(cw, tabs(tab, 3), "Hidden: false,")
-				}
-				fmt.Fprintln(cw, tabs(tab, 2), "},")
 			case reflect.TypeOf([]int{}):
-				fmt.Fprintln(cw, tabs(tab, 2), "cli.IntSliceFlag{")
-				fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-				fmt.Fprintln(fw, "\t", "if c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-				fmt.Fprintln(fw, "\t\t", getCamelCase(getTag(v.Type(), i)), ":=", "c.IntSlice(\""+getTag(v.Type(), i)+"\")")
-				fmt.Fprintln(fw, "\t\t", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, " = ", getCamelCase(getTag(v.Type(), i)))
-				fmt.Fprintln(fw, "\t", "}")
-
-				fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"(Int...)\\t"+getUsage(v.Type(), i)+"\",")
-				if isHidden(v.Type(), i) {
-					fmt.Fprintln(cw, tabs(tab, 3), "Hidden: true,")
-				} else {
-					fmt.Fprintln(cw, tabs(tab, 3), "Hidden: false,")
-				}
-				fmt.Fprintln(cw, tabs(tab, 2), "},")
+				setFunc(cw, fw, v, i, tab, prefix, "IntSlice", "Int...")
 			default:
 				fmt.Fprintln(os.Stderr, "ignoring", prefix+v.Type().Field(i).Name, "of type", v.Field(i).Type().String())
 			}
 		case reflect.String:
-			fmt.Fprintln(cw, tabs(tab, 2), "cli.StringFlag{")
-			fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-			fmt.Fprintln(fw, "\t", "if c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-			fmt.Fprintln(fw, "\t\t", getCamelCase(getTag(v.Type(), i)), ":=", "c.String(\""+getTag(v.Type(), i)+"\")")
-			fmt.Fprintln(fw, "\t\t", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, " = ", getCamelCase(getTag(v.Type(), i)))
-			fmt.Fprintln(fw, "\t", "}")
-
-			fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"(Str)\\t"+getUsage(v.Type(), i)+"\",")
-			if isHidden(v.Type(), i) {
-				fmt.Fprintln(cw, tabs(tab, 3), "Hidden: true,")
-			} else {
-				fmt.Fprintln(cw, tabs(tab, 3), "Hidden: false,")
-			}
-			fmt.Fprintln(cw, tabs(tab, 2), "},")
+			setFunc(cw, fw, v, i, tab, prefix, "String", "Str")
 		case reflect.Int:
-			fmt.Fprintln(cw, tabs(tab, 2), "cli.IntFlag{")
-			fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-			fmt.Fprintln(fw, "\t", "if c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-			fmt.Fprintln(fw, "\t\t", getCamelCase(getTag(v.Type(), i)), ":=", "c.Int(\""+getTag(v.Type(), i)+"\")")
-			fmt.Fprintln(fw, "\t\t", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, " = ", getCamelCase(getTag(v.Type(), i)))
-			fmt.Fprintln(fw, "\t", "}")
-
-			fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"(Int)\\t"+getUsage(v.Type(), i)+"\",")
-			if isHidden(v.Type(), i) {
-				fmt.Fprintln(cw, tabs(tab, 3), "Hidden: true,")
-			} else {
-				fmt.Fprintln(cw, tabs(tab, 3), "Hidden: false,")
-			}
-			fmt.Fprintln(cw, tabs(tab, 2), "},")
+			setFunc(cw, fw, v, i, tab, prefix, "Int", "Int")
 		case reflect.Bool:
-			fmt.Fprintln(cw, tabs(tab, 2), "cli.BoolFlag{")
-			fmt.Fprintln(cw, tabs(tab, 3), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-			fmt.Fprintln(fw, "\t", "if c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-			fmt.Fprintln(fw, "\t\t", getCamelCase(getTag(v.Type(), i)), ":=", "c.Bool(\""+getTag(v.Type(), i)+"\")")
-			fmt.Fprintln(fw, "\t\t", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, " = ", getCamelCase(getTag(v.Type(), i)))
-			fmt.Fprintln(fw, "\t", "}")
-
-			fmt.Fprintln(cw, tabs(tab, 3), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
-			if isHidden(v.Type(), i) {
-				fmt.Fprintln(cw, tabs(tab, 3), "Hidden: true,")
-			} else {
-				fmt.Fprintln(cw, tabs(tab, 3), "Hidden: false,")
-			}
-			fmt.Fprintln(cw, tabs(tab, 2), "},")
+			setFunc(cw, fw, v, i, tab, prefix, "Bool", "Bool")
 		default:
 			//M[v.Field(i).Kind()] = append(M[v.Field(i).Kind()], prefix+v.Type().Field(i).Name)
 		}
 	}
 
-	fmt.Fprintln(fw, "\t", "jb, _ := json.Marshal(config); ioutil.WriteFile(\"/tmp/config.json\", jb, 0666)")
-	fmt.Fprintln(fw, "\t", "jb, _ = json.Marshal(nodeConfig); ioutil.WriteFile(\"/tmp/nodeConfig.json\", jb, 0666)")
+	if prefixOrigin[prefix] == "node" {
+		fmt.Fprintln(fw, "\t", "return clusterManager.SetNodeConf(config)")
+	} else {
+		fmt.Fprintln(fw, "\t", "return clusterManager.SetClusterConf(config)")
+	}
 	fmt.Fprintln(fw, "}")
 	fmt.Fprintln(fw)
 	fmt.Fprintln(cw, tabs(tab, 1), "},")
@@ -204,7 +239,21 @@ func printFields(v reflect.Value, hidden bool, prefix, usage, description, tab s
 	fmt.Fprintln(cw, tabs(tab, 3), "Description:", "\"Show values\",")
 	fmt.Fprintln(cw, tabs(tab, 3), "Action:", getCamelCase("show_"+prefix+"_values,"))
 
-	fmt.Fprintln(fw, "func", getCamelCase("show_"+prefix+"_values(c *cli.Context) {"))
+	fmt.Fprintln(fw, "func", getCamelCase("show_"+prefix+"_values(c *cli.Context) error {"))
+	if prefixOrigin[prefix] == "node" {
+		fmt.Fprintln(fw, "\t", "config, err := clusterManager.GetNodeConf()")
+		fmt.Fprintln(fw, "\t", "if err != nil {")
+		fmt.Fprintln(fw, "\t\t", "return err")
+		fmt.Fprintln(fw, "\t", "}")
+	} else {
+		fmt.Fprintln(fw, "\t", "config, err := clusterManager.GetClusterConf()")
+		fmt.Fprintln(fw, "\t", "if err != nil {")
+		fmt.Fprintln(fw, "\t\t", "return err")
+		fmt.Fprintln(fw, "\t", "}")
+	}
+	fmt.Fprintln(fw, "\t", "if c.GlobalBool(\"json\") {")
+	fmt.Fprintln(fw, "\t\t", "return printJson("+prefix2VarName[prefix]+")")
+	fmt.Fprintln(fw, "\t", "}")
 	fmt.Fprintln(cw, tabs(tab, 3), "Flags: []cli.Flag{")
 	fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
 	fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\"all, a\",")
@@ -217,87 +266,23 @@ func printFields(v reflect.Value, hidden bool, prefix, usage, description, tab s
 		case reflect.Slice:
 			switch field.Type() {
 			case reflect.TypeOf([]string{}):
-				fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
-				fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-				fmt.Fprintln(fw, "\t", "if c.IsSet(\"all\") || c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-				fmt.Fprintln(fw, "\t\t", "fmt.Println(", "\""+v.Type().Field(i).Name+":\",", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, ")")
-				fmt.Fprintln(fw, "\t", "}")
-
-				fmt.Fprintln(cw, tabs(tab, 5), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
-				if isHidden(v.Type(), i) {
-					fmt.Fprintln(cw, tabs(tab, 5), "Hidden: true,")
-				} else {
-					fmt.Fprintln(cw, tabs(tab, 5), "Hidden: false,")
-				}
-				fmt.Fprintln(cw, tabs(tab, 4), "},")
+				getFunc(cw, fw, v, i, tab, prefix)
 			case reflect.TypeOf([]int{}):
-				fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
-				fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-				fmt.Fprintln(fw, "\t", "if c.IsSet(\"all\") || c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-				fmt.Fprintln(fw, "\t\t", "fmt.Println(", "\""+v.Type().Field(i).Name+":\",", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, ")")
-				fmt.Fprintln(fw, "\t", "}")
-
-				fmt.Fprintln(cw, tabs(tab, 5), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
-				if isHidden(v.Type(), i) {
-					fmt.Fprintln(cw, tabs(tab, 5), "Hidden: true,")
-				} else {
-					fmt.Fprintln(cw, tabs(tab, 5), "Hidden: false,")
-				}
-				fmt.Fprintln(cw, tabs(tab, 4), "},")
+				getFunc(cw, fw, v, i, tab, prefix)
 			default:
 				fmt.Fprintln(os.Stderr, "ignoring", prefix+v.Type().Field(i).Name, "of type", v.Field(i).Type().String())
 			}
 		case reflect.String:
-			fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
-			fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-			fmt.Fprintln(fw, "\t", "if c.IsSet(\"all\") || c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-			fmt.Fprintln(fw, "\t\t", "fmt.Println(", "\""+v.Type().Field(i).Name+":\",", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, ")")
-			fmt.Fprintln(fw, "\t", "}")
-
-			fmt.Fprintln(cw, tabs(tab, 5), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
-			if isHidden(v.Type(), i) {
-				fmt.Fprintln(cw, tabs(tab, 5), "Hidden: true,")
-			} else {
-				fmt.Fprintln(cw, tabs(tab, 5), "Hidden: false,")
-			}
-			fmt.Fprintln(cw, tabs(tab, 4), "},")
+			getFunc(cw, fw, v, i, tab, prefix)
 		case reflect.Int:
-			fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
-			fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-			fmt.Fprintln(fw, "\t", "if c.IsSet(\"all\") || c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-			fmt.Fprintln(fw, "\t\t", "fmt.Println(", "\""+v.Type().Field(i).Name+":\",", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, ")")
-			fmt.Fprintln(fw, "\t", "}")
-
-			fmt.Fprintln(cw, tabs(tab, 5), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
-			if isHidden(v.Type(), i) {
-				fmt.Fprintln(cw, tabs(tab, 5), "Hidden: true,")
-			} else {
-				fmt.Fprintln(cw, tabs(tab, 5), "Hidden: false,")
-			}
-			fmt.Fprintln(cw, tabs(tab, 4), "},")
+			getFunc(cw, fw, v, i, tab, prefix)
 		case reflect.Bool:
-			fmt.Fprintln(cw, tabs(tab, 4), "cli.BoolFlag{")
-			fmt.Fprintln(cw, tabs(tab, 5), "Name:", "\""+getTag(v.Type(), i)+"\",")
-
-			fmt.Fprintln(fw, "\t", "if c.IsSet(\"all\") || c.IsSet(\""+getTag(v.Type(), i)+"\") {")
-			fmt.Fprintln(fw, "\t\t", "fmt.Println(", "\""+v.Type().Field(i).Name+":\",", prefix2VarName[prefix]+"."+v.Type().Field(i).Name, ")")
-			fmt.Fprintln(fw, "\t", "}")
-
-			fmt.Fprintln(cw, tabs(tab, 5), "Usage: \"(Bool)\\t"+getUsage(v.Type(), i)+"\",")
-			if isHidden(v.Type(), i) {
-				fmt.Fprintln(cw, tabs(tab, 5), "Hidden: true,")
-			} else {
-				fmt.Fprintln(cw, tabs(tab, 5), "Hidden: false,")
-			}
-			fmt.Fprintln(cw, tabs(tab, 4), "},")
+			getFunc(cw, fw, v, i, tab, prefix)
 		default:
 			//M[v.Field(i).Kind()] = append(M[v.Field(i).Kind()], prefix+v.Type().Field(i).Name)
 		}
 	}
+	fmt.Fprintln(fw, "\t", "return nil")
 	fmt.Fprintln(fw, "}")
 	fmt.Fprintln(fw)
 	fmt.Fprintln(cw, tabs(tab, 3), "},")
@@ -308,8 +293,9 @@ func printFields(v reflect.Value, hidden bool, prefix, usage, description, tab s
 		config := new(osdconfig.NodeConfig)
 		config.Network = new(osdconfig.NetworkConfig)
 		config.Storage = new(osdconfig.StorageConfig)
-		prefix2VarName["nodeConfig"] = "nodeConfig"
-		printFields(reflect.Indirect(reflect.ValueOf(config)), false, "nodeConfig", "node usage", "node description", tabs(tab, 2))
+		prefix2VarName["node"] = "config"
+		prefixOrigin["node"] = "node"
+		printFields(reflect.Indirect(reflect.ValueOf(config)), false, "node", "node usage", "node description", tabs(tab, 2))
 	}
 
 	// subcommand based on nested struct
@@ -317,12 +303,12 @@ func printFields(v reflect.Value, hidden bool, prefix, usage, description, tab s
 		field := v.Field(i)
 		switch k := field.Kind(); k {
 		case reflect.Ptr:
-			if !field.IsNil() {
+			if !field.IsNil() && isEnabled(v.Type(), i) {
 				prefix2VarName[getTag(v.Type(), i)] = prefix2VarName[prefix] + "." + v.Type().Field(i).Name
+				prefixOrigin[getTag(v.Type(), i)] = prefixOrigin[prefix]
 				printFields(field.Elem(), isHidden(v.Type(), i), getTag(v.Type(), i), getUsage(v.Type(), i), getDescription(v.Type(), i), tabs(tab, 2))
 			}
 		default:
-			//M[v.Field(i).Kind()] = append(M[v.Field(i).Kind()], prefix+v.Type().Field(i).Name)
 		}
 	}
 
@@ -373,6 +359,21 @@ func isHidden(t reflect.Type, n int) bool {
 	return false
 }
 
+func isEnabled(t reflect.Type, n int) bool {
+	if n >= t.NumField() {
+		panic("index out of bounds")
+	}
+
+	tag := t.Field(n).Tag.Get("enable")
+	if tag == "true" {
+		return true
+	}
+	if tag == "false" {
+		return false
+	}
+	return true
+}
+
 func getUsage(t reflect.Type, n int) string {
 	if n > t.NumField() {
 		panic("index out of bounds")
@@ -413,7 +414,7 @@ func getTag(t reflect.Type, n int) string {
 func getCamelCase(input string) string {
 	upper := []byte(strings.ToUpper(input))
 	out := make([]byte, 0, 0)
-	for i :=0; i < len(input); i++ {
+	for i := 0; i < len(input); i++ {
 		if i > 0 && input[i] == '_' {
 			continue
 		}
